67106e878c5a2461d49a351f6ecd3db37f496766
Pranav <38887390+thegamerhat@users.noreply.github.com>
2023-04-03 15:02:40 +0530

    POC AI speech recognition

--- task_genie.py
+++ task_genie.py
@@ -2,26 +2,35 @@
 
 import speech_recognition as sr
 import datetime
-from time import ctime
+import time
 import os
-from playsound import playsound
+# from playsound import playsound
+import requests
+import json
 import openai
+import pyautogui
 from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env file
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
 # openai.api_key = os.environ["OPENAI_API_KEY"]
 
 doneListening = './audio/done_listening.wav'
+
+ChatCompletionsURL = 'https://api.openai.com/v1/chat/completions'
+OpenAI_Headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {OPENAI_API_KEY}'
+}
 
 # Fill your own details
 
 yourName = 'Master'  # enter your name
 
-
-def beep():
-    playsound(doneListening)
+# def beep():
+#     playsound(doneListening)
 
 
 def takeCommand():
@@ -56,29 +65,59 @@ def respond(query):
             return 0
 
         elif ('bye' or 'exit' or 'shut down' or 'shutdown') in query:
-            beep()
+            # beep()
             print("Turning off!")
             exit()
 
         # generate a response using OpenAI ChatGPT API
         prompt = f"What is your opinion on {query}?"
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                  messages=[{
-                                                      "role": "user",
-                                                      "content": prompt
-                                                  }])
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role":
+                "system",
+                "content":
+                "You are a programming assistant. Respond only in javascript code. Keep all other prose as javascript comments. Strictly output nothing else, other than javascript code. Do not include markdown code syntax."
+            }, {
+                "role": "user",
+                "content": query
+            }])
+        response = completion.choices[0].message.content.strip()
 
-        print(completion)
+        # data = {
+        #     'model': 'gpt-3.5-turbo',
+        #     'stream': True,
+        #     'messages': [{
+        #         'role': 'user',
+        #         'content': query
+        #     }]
+        # }
+
+        # r = requests.post(ChatCompletionsURL,
+        #                            headers=OpenAI_Headers,
+        #                            data=json.dumps(data))
+
+        # response = r.content
+        print(response)
+
+        pyautogui.typewrite(response)
+
+        # for char in response:
+        # pyautogui.press(char)
+        # time.sleep(0.1)  # add a small delay between each key press
+
+        return
 
     except Exception as e:
-        beep()
-        print("sorry, i didn't get what you said.")
-        print('\nPlease say again...\n')
+        #
+        print("Exception occured:")
+        print(e)
+        print('\nPlease try again...\n')
 
 
 if __name__ == "__main__":
-    beep()
+    # beep()
     while True:
-        beep()
+        # beep()
         order = takeCommand()
         respond(order)
